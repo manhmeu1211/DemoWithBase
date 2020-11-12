acdaaef1e4790821c001ee11df28860742de70c2
ManhLD <manhld@rikkeisoft.com>
2020-11-12 14:49:26 +0700

    fourth commit

--- BaseAFAPI/Application/ViewController.swift
+++ BaseAFAPI/Application/ViewController.swift
@@ -8,38 +8,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
-    var model : [NewsModel] = []
-    
+        
     override func viewDidLoad() {
         super.viewDidLoad()
-        getNewsData()
-        login()
-        view.backgroundColor = .white
-    }
     
-    private func getNewsData() {
-        SampleGetNewsAPI(paramss: ["pageIndex": 1, "pageSize": 10]).execute { (response) in
-            switch response {
-            case .success(let news):
-                self.model = news.response?.news ?? []
-                print(self.model)
-            case .failure(let err):
-                print(err.localizedDescription)
-            }
-        }
     }
-    
-    private func login() {
-        SampleLoginAPI(paramss: ["email": "fullterdev@gmail.com", "password": "25251325"]).execute { (response) in
-            print(response)
-            switch response {
-            case .success(let loginRes):
-                print(loginRes)
-            case .failure(let err):
-                print(err.localizedDescription)
-            }
-        }
-    }
+
 }
 
--- BaseAFAPI/ViewControllers/TabbarVC/ESTabbar.swift
+++ BaseAFAPI/ViewControllers/TabbarVC/ESTabbar.swift
@@ -17,8 +17,8 @@ class CustomTabbar {
         tabBarController.tabBar.backgroundColor =  .white
         let v1 = HomeVC()
         let v2 = EventsVC()
-        v1.tabBarItem = ESTabBarItem.init(ExampleBasicContentView(), title: "News", image: UIImage(named: "icon-home"), selectedImage: UIImage(named: "icon-home"))
-        v2.tabBarItem = ESTabBarItem.init(ExampleBasicContentView(), title: "Events", image: UIImage(named: "home"), selectedImage: UIImage(named: "home_1"))
+        v1.tabBarItem = ESTabBarItem.init(ExampleIrregularityBasicContentView(), title: "News", image: UIImage(named: "icon-home"), selectedImage: UIImage(named: "icon-home"))
+        v2.tabBarItem = ESTabBarItem.init(ExampleIrregularityBasicContentView(), title: "Events", image: UIImage(named: "home"), selectedImage: UIImage(named: "home_1"))
         tabBarController.viewControllers = [v1, v2]
         return tabBarController
     }
